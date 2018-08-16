ab695b23a0fa3119b88280437672a1bd3e3cf33c
i826181 <stan.stadelman@sap.com>
2018-08-15 21:46:14 -0700

    Initial Commit

---
+++ StellarJayTests/StellarJayTests.swift
@@ -0,0 +1,34 @@
+//
+//  StellarJayTests.swift
+//  StellarJayTests
+//
+//  Created by Stadelman, Stan on 8/15/18.
+//  Copyright © 2018 sstadelman. All rights reserved.
+//
+
+import XCTest
+@testable import StellarJay
+
+class StellarJayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
